b6a87170e675b5399808794f5b1cf8bf05b6727f
BG1supernode333 <bg.supernode@gmail.com>
2014-09-18 17:44:59 +0900

    add MFC AfxMessageBox

---
+++ MFC/AfxMessageBox/Src/AfxMessageBox/AfxMessageBox/AfxMessageBox.cpp
@@ -0,0 +1,9 @@
+#include <afxwin.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+
+	AfxMessageBox("AfxMessageBox", MB_OK | MB_ICONASTERISK);
+
+	return 0;
+
+}
