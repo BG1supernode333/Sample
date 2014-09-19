67b915832a5134f373a3bf3e91c556cb8aa3f657
BG1supernode333 <bg.supernode@gmail.com>
2014-09-19 11:47:21 +0900

    add android

---
+++ AndroidSDK/Activity/src/Activity/Activity/src/com/bgstation0/sample/androidsdk/activity/ActivityMainActivity.java
@@ -0,0 +1,15 @@
+package com.bgstation0.sample.androidsdk.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_main);
+    }
+}
