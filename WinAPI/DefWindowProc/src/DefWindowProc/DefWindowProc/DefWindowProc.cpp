7cf70604abb3ec34a766eeaae8dc14f6e3ad1919
BG1supernode333 <bg.supernode@gmail.com>
2014-09-26 12:22:00 +0900

    add DefWindowProc

---
+++ WinAPI/DefWindowProc/src/DefWindowProc/DefWindowProc/DefWindowProc.cpp
@@ -0,0 +1,62 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+
+	HWND hWnd;
+	MSG msg;
+	WNDCLASS wc;
+
+	wc.lpszClassName = "DefWindowProc";
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+
+	if (!RegisterClass(&wc)){
+
+		MessageBox(NULL, "RegisterClass failed!", "DefWindowProc", MB_OK | MB_ICONHAND);
+		return -1;
+
+	}
+
+	hWnd = CreateWindow("DefWindowProc", "DefWindowProc", WS_OVERLAPPEDWINDOW, 100, 100, 640, 480, NULL, NULL, hInstance, NULL);
+	if (hWnd == NULL){
+
+		MessageBox(NULL, "CreateWindow failed!", "DefWindowProc", MB_OK | MB_ICONHAND);
+		return -2;
+
+	}
+	
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (1){
+
+		GetMessage(&msg, NULL, 0, 0);
+		if (msg.message == WM_NCLBUTTONDOWN && msg.wParam == HTCLOSE){
+
+			break;
+
+		}
+
+		DispatchMessage(&msg);
+
+	}
+
+	DestroyWindow(hWnd);
+
+	return 0;
+
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+}
