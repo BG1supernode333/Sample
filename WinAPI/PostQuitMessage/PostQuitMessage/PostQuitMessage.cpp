1ea1f794da5c3d4082addc9555bd76beff1dec2a
BG1supernode333 <bg.supernode@gmail.com>
2014-09-29 14:25:40 +0900

    add PostQuitMessage

---
+++ WinAPI/PostQuitMessage/PostQuitMessage/PostQuitMessage.cpp
@@ -0,0 +1,68 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+
+	HWND hWnd;
+	MSG msg;
+	WNDCLASS wc;
+
+	wc.lpszClassName = "PostQuitMessage";
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+
+	if (!RegisterClass(&wc)){
+
+		MessageBox(NULL, "RegisterClass failed!", "PostQuitMessage", MB_OK | MB_ICONHAND);
+		return -1;
+
+	}
+
+	hWnd = CreateWindow("PostQuitMessage", "PostQuitMessage", WS_OVERLAPPEDWINDOW, 100, 100, 640, 480, NULL, NULL, hInstance, NULL);
+	if (hWnd == NULL){
+
+		MessageBox(NULL, "CreateWindow failed!", "PostQuitMessage", MB_OK | MB_ICONHAND);
+		return -2;
+
+	}
+	
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (GetMessage(&msg, NULL, 0, 0) > 0){
+
+		DispatchMessage(&msg);
+
+	}
+
+	DestroyWindow(hWnd);
+
+	return 0;
+
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+
+	switch (uMsg){
+
+		case WM_DESTROY:
+
+			PostQuitMessage(0);
+			break;
+
+		default:
+
+			break;
+
+	}
+
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+}
