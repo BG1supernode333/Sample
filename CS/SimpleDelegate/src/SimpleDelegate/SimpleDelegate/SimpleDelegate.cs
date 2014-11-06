8da0445c6a0be7f27e0800402e0a867a3d274bc0
BG1supernode333 <bg.supernode@gmail.com>
2014-11-06 10:42:04 +0900

    add SimpleDelegate

---
+++ CS/SimpleDelegate/src/SimpleDelegate/SimpleDelegate/SimpleDelegate.cs
@@ -0,0 +1,39 @@
+using System;
+
+class SimpleDelegate
+{
+    delegate long FactorialDelegate(long n);
+    static void Main()
+    {
+        long result;
+
+        result = Factorial(20);
+
+        Console.WriteLine("Final result = " + result);
+    }
+    static long Factorial(long n)
+    {
+        long x = 1;
+
+        FactorialDelegate fd = FactorialCallback;
+
+        for (long i = 2; i <= n; i++)
+        {
+            x = x * i;
+        }
+
+        long t;
+
+        t = fd(x);
+
+        Console.WriteLine("Delegate after result = " + t);
+
+        return t;
+    }
+    static long FactorialCallback(Int64 result)
+    {
+        Console.WriteLine("Callback inner result = " + result);
+
+        return result;
+    }
+}
