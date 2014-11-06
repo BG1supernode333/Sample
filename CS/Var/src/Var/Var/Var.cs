ec0d3d090c129bbfa26481f3c01160db2df248bc
BG1supernode333 <bg.supernode@gmail.com>
2014-11-06 17:42:20 +0900

    ad

---
+++ CS/Var/src/Var/Var/Var.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Var
+{
+    static void Main()
+    {
+        var ils = new List<int>();
+
+        ils.Add(5);
+        ils.Add(2);
+        ils.Add(4);
+        ils.Add(1);
+        ils.Add(3);
+
+        foreach (var e in ils)
+        {
+            Console.WriteLine("e = " + e);
+        }
+    }
+}
