3650e0cb53d6de7c734e96c93599587a77d707a0
BG1supernode333 <bg.supernode@gmail.com>
2014-11-07 11:52:57 +0900

    add AnonymousType

---
+++ CS/AnonymousType/src/AnonymousType/AnonymousType/AnonymousType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class AnonymousType
+{
+    static void Main()
+    {
+        var prof = new { Name = "Taro", Age = 20, Address = "Tokyo" };
+        
+        Console.WriteLine("prof.Name = " + prof.Name);
+        Console.WriteLine("prof.Age = " + prof.Age);
+        Console.WriteLine("prof.Address = " + prof.Address);
+    }
+}
