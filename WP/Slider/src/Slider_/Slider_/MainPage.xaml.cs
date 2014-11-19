d5d777254ee6d0bf59e1d1e937d1d2ae0b0f73d8
BG1supernode333 <bg.supernode@gmail.com>
2014-11-19 10:23:34 +0900

    add Slider_

---
+++ WP/Slider/src/Slider_/Slider_/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Slider_
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider slider = (Slider)sender;
+            int value;
+
+            value = (int)slider.Value;
+            textBlock2.Text = value.ToString();
+        }
+    }
+}
