d09f0d3a64dab0ff0996ef5649b939113b1ad73a
BG1supernode333 <bg.supernode@gmail.com>
2014-11-20 17:27:43 +0900

    add IsolatedStorageFile_

---
+++ WP/IsolatedStorageFile_/src/IsolatedStorageFile_/IsolatedStorageFile_/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace IsolatedStorageFile_
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageFile storage = null;
+            IsolatedStorageFileStream stream = null;
+            try
+            {
+                storage = IsolatedStorageFile.GetUserStoreForApplication();
+                stream = storage.CreateFile(textBox1.Text);
+                if (storage.FileExists(textBox1.Text))
+                {
+                    MessageBox.Show("IsolatedStorageFile_ [" + textBox1.Text + "] Created!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+            finally
+            {
+                if (storage.FileExists(textBox1.Text))
+                {
+                    stream.Close();
+                    storage.DeleteFile(textBox1.Text);
+                }
+            }
+        }
+    }
+}
