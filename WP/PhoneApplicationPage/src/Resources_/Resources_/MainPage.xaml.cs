d5b63d6c30334700ee447411a318a2eac31f43cb
BG1supernode333 <bg.supernode@gmail.com>
2014-11-20 10:59:54 +0900

    add Resources_

---
+++ WP/PhoneApplicationPage/src/Resources_/Resources_/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Resources_
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button1 Clicked!");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button2 Clicked!");
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button3 Clicked!");
+        }
+    }
+}
