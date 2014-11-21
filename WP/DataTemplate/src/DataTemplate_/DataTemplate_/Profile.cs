001cd3eafa0391f862d5e5f10ac0254ffc6415c9
BG1supernode333 <bg.supernode@gmail.com>
2014-11-21 18:51:35 +0900

    add DataTemplate_

---
+++ WP/DataTemplate/src/DataTemplate_/DataTemplate_/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DataTemplate_
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+    public class ProfileList
+    {
+        public List<Profile> Profiles { get; set; }
+        public ProfileList()
+        {
+            Profiles = new List<Profile>
+            {
+                new Profile{ Name = "Taro", Age = 20, Address = "Tokyo"},
+                new Profile{ Name = "Jiro", Age = 18, Address = "Osaka"},
+                new Profile{ Name = "Saburo", Age = 17, Address = "Nagoya"},
+                new Profile{ Name = "Shiro", Age = 16, Address = "Fkuoka"},
+                new Profile{ Name = "Goro", Age = 15, Address = "Sapporo"}
+            };
+        }
+    }
+}
