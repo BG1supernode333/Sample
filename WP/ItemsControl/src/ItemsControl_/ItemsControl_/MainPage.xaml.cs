a141210c7be1f855dd5c8e988512e8e69b0b1e98
BG1supernode333 <bg.supernode@gmail.com>
2014-11-21 17:11:41 +0900

    fixed Tag Position

--- WP/ItemsControl/src/ItemsControl_/ItemsControl_/MainPage.xaml.cs
+++ WP/ItemsControl/src/ItemsControl_/ItemsControl_/MainPage.xaml.cs
@@ -19,6 +19,8 @@ namespace ItemsControl_
         public MainPage()
         {
             InitializeComponent();
+            itemctrl1.Items.Add("test");
+            itemctrl1.Items.Add("test2");
         }
     }
 }
