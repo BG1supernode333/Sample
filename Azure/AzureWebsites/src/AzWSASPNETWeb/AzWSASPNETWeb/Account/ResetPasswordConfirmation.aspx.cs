c36e48a5236801f246ec3a90a058fed4dfaa881d
BG1supernode333 <bgother01@gmail.com>
2014-12-03 13:52:01 +0900

    add AzWSASPNETWeb

---
+++ Azure/AzureWebsites/src/AzWSASPNETWeb/AzWSASPNETWeb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AzWSASPNETWeb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Azure/AzureWebsites/src/AzWSASPNETWeb/AzWSASPNETWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzWSASPNETWeb.Startup))]
+namespace AzWSASPNETWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
